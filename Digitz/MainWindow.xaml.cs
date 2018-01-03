32215d875ad5882419c5accf339041d44d50d32f
sethjuarez <me@sethjuarez.com>
2018-01-03 15:23:52 -0800

    created tensor from InkCanvas

---
+++ Digitz/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TensorFlow;
+
+namespace Digitz
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private TFTensor GetWrittenDigit(int size)
+        {
+            RenderTargetBitmap b = new RenderTargetBitmap(
+                (int)inkCanvas.ActualWidth, (int)inkCanvas.ActualHeight,
+                96d, 96d, PixelFormats.Default
+            );
+
+            b.Render(inkCanvas);
+            var bitmap = new WriteableBitmap(b)
+                            .Resize(size, size, WriteableBitmapExtensions.Interpolation.Bilinear);
+
+            float[] data = new float[size * size];
+            for (int x = 0; x < bitmap.PixelWidth; x++)
+            {
+                for (int y = 0; y < bitmap.PixelHeight; y++)
+                {
+                    var color = bitmap.GetPixel(x, y);
+                    data[y * bitmap.PixelWidth + x] = 255 - ((color.R + color.G + color.B) / 3);
+                }
+            }
+            
+            // sanity check
+            Console.Write(Stringify(data));
+
+            return new TFTensor(data);
+        }
+
+        private void recognizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            var tensor = GetWrittenDigit(28);
+            
+        }
+
+        private string Stringify(float[] data)
+        {
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < data.Length; i++)
+            {
+                if (i == 0) sb.Append("{\r\n\t");
+                else if (i % 28 == 0)
+                    sb.Append("\r\n\t");
+                sb.Append($"{data[i],3:##0}, ");
+                
+            }
+            sb.Append("\r\n}\r\n");
+            return sb.ToString();
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.Strokes.Clear();
+        }
+    }
+}
